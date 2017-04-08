ee44ad6752fca5c875f21bd740d6274277e0459d
Dinesh Weerapurage <dinesh.weerapurage@pearson.com>
2017-04-08 06:26:10 -0600

    Added a logger and routes

--- handlers.go
+++ handlers.go
@@ -34,5 +34,6 @@ func GetCommits(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(nc)
 }
